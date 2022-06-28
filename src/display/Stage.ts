50171d47993e15ff29fe6f45c583f48bd9abb407
kevin <tyrocoder@163.com>
2022-06-28 21:15:29 +0800

    mod: stage

--- src/display/Stage.ts
+++ src/display/Stage.ts
@@ -1,4 +1,6 @@
+import CanvasRenderer from "../renderer/CanvasRenderer";
 import Renderer from "../renderer/Renderer";
+import { RENDER_TYPE } from "../utils/Constants";
 import Container from "./Container";
 
 export interface IViewPort {
@@ -16,11 +18,31 @@ export default class Stage extends Container {
 
   protected _instanceType: string = 'Stage'
 
-  constructor() {
+  constructor(
+    canvas: HTMLCanvasElement,
+    designWidth: number,
+    designHeight: number,
+    viewWidth: number = document.body.clientWidth,
+    viewHeight: number = document.body.clientHeight,
+    renderType: RENDER_TYPE = RENDER_TYPE.CANVAS,
+  ) {
     super()
+
+    this._initRenderer(canvas, renderType)
   }
 
+  /**
+   * 判断目标对象是否是 Stage
+   * @param val 对象
+   * @returns 
+   */
   static isStage(val: any) {
     return val instanceof Stage
+  }
+
+  private _initRenderer(canvas: HTMLCanvasElement, renderType: RENDER_TYPE) {
+    if (renderType === RENDER_TYPE.CANVAS) {
+      this.renderer = new CanvasRenderer()
+    }
   }
 }
---
+++ src/utils/Constants.ts
@@ -0,0 +1,4 @@
+export enum RENDER_TYPE {
+  CANVAS,
+  WEBGL,
+}
